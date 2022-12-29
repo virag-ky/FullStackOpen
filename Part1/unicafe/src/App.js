323a97f02a51ed700d1cf11eccc0b175fd775040
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-29 16:53:00 +0200

    Add unicafe part 1 solution

---
+++ Part1/unicafe/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState } from 'react';
+import Button from './components/Button';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGood = () => setGood(good + 1);
+  const addNeutral = () => setNeutral(neutral + 1);
+  const addBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={addGood} text="good" />
+      <Button onClick={addNeutral} text="neutral" />
+      <Button onClick={addBad} text="bad" />
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
+
+export default App;
