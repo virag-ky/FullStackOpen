6706926f3a22f682bef3ae1967a88656440623f4
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-30 13:54:47 +0200

    Add course information step 7 and 8 solution

--- Part2/course-information/src/App.js
+++ Part2/course-information/src/App.js
@@ -1,16 +1,5 @@
 import Course from './components/Course';
 
-// const Total = (props) => {
-//   return (
-//     <p>
-//       Number of exercises{' '}
-//       {props.parts[0].exercises +
-//         props.parts[1].exercises +
-//         props.parts[2].exercises}
-//     </p>
-//   );
-// };
-
 const App = () => {
   const course = {
     id: 1,
