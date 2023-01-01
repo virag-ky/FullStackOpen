e912223a6cafccbd1eab2a0f6967d2b5ec2db9f9
Virag Kormoczy <vkormoczy@gmail.com>
2023-01-01 14:42:53 +0200

    Add json server

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 import Content from './components/Content';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchValue, setSearchValue] = useState('');
@@ -47,15 +43,21 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  React.useEffect(() => {
-    const results = persons.filter((person) =>
-      person.name.includes(searchValue.toUpperCase() && searchValue)
-    );
-    const copy = results.map((result) => {
-      return { name: result.name, number: result.number, id: result.id };
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((result) => {
+      setPersons(result.data);
     });
-    setFiltered(copy);
-  }, [searchValue]);
+  }, []);
+
+  // React.useEffect(() => {
+  //   const results = persons.filter((person) =>
+  //     person.name.includes(searchValue.toUpperCase() && searchValue)
+  //   );
+  //   const copy = results.map((result) => {
+  //     return { name: result.name, number: result.number, id: result.id };
+  //   });
+  //   setFiltered(copy);
+  // }, [searchValue]);
 
   return (
     <div>
