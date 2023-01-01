3c0fe541fb91aec28cf543c78d6ac2ec7319f822
Virag Kormoczy <vkormoczy@gmail.com>
2023-01-01 17:55:21 +0200

    Add solution for step 1

--- Part2/data_for_countries/src/App.js
+++ Part2/data_for_countries/src/App.js
@@ -5,33 +5,55 @@ import axios from 'axios';
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchValue, setSearchValue] = useState('');
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((response) => {
-      const countryNames = response.data.map((data) => {
-        return data.name.common;
-      });
-      //console.log(response.data);
+      const countryNames = response.data;
       setCountries(countryNames);
     });
   }, []);
 
   const getResults = (event) => {
     setSearchValue(event.target.value);
+    setLoaded(true);
   };
 
   const displayCountries = () => {
-    return countries
-      .filter((country) => {
-        if (searchValue === '') {
-          return country;
-        } else if (
-          country.toLowerCase().includes(searchValue.toLocaleLowerCase())
-        ) {
-          return country;
-        }
-      })
-      .map((item) => <p key={item}>{item}</p>);
+    const filtered = countries.filter((country) => {
+      if (searchValue === '') {
+        setLoaded(false);
+        return country;
+      } else if (
+        country.name.common
+          .toLowerCase()
+          .includes(searchValue.toLocaleLowerCase())
+      ) {
+        return country;
+      }
+    });
+
+    if (filtered.length > 10) {
+      return <p>Too many matches, specify another filter.</p>;
+    } else if (filtered.length <= 10 && filtered.length > 1) {
+      return filtered.map((item) => <p key={item.id}>{item.name.common}</p>);
+    } else {
+      return filtered.map((item) => (
+        <div key={item.name.official}>
+          <h1>{item.name.common}</h1>
+          <span>Capital: {item.capital}</span>
+          <br />
+          <span>Area: {item.area}</span>
+          <h2>Languages:</h2>
+          <ul>
+            {Object.values(item.languages).map((language) => (
+              <li key={language}>{language}</li>
+            ))}
+          </ul>
+          <img alt="country flag" src={item.flags.png} />
+        </div>
+      ));
+    }
   };
 
   return (
@@ -39,7 +61,7 @@ const App = () => {
       <label>
         Find countries <input type="text" onChange={getResults} />
       </label>
-      <div>{displayCountries()}</div>
+      <div>{loaded ? displayCountries() : ''}</div>
     </div>
   );
 };
